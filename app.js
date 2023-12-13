c6ed31379ff51470a77064422a0b50b4cc1d1daf
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-12-13 03:50:58 +0000

    iniciando proyecto movies api

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+
+const app=express();
+app.use(express.json());
+app.disable('x-powered-by');
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+}   );
+
+app.listen(3000,()=>{
+    console.log(`Server is running on port http://localhost:3000`);
+}
+);
